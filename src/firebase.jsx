ec8267ed4d658165d27be01599b35ed66689a4f0
NineLEL <74411437+NineLEL@users.noreply.github.com>
2022-11-14 22:30:24 +0700

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA4jJwxUvlqnGq5QPNyenX8CrWgwqozp5c",
-  authDomain: "static-lens-299403.firebaseapp.com",
-  projectId: "static-lens-299403",
-  storageBucket: "static-lens-299403.appspot.com",
-  messagingSenderId: "705399504086",
-  appId: "1:705399504086:web:f80da540144bd8ef935d77",
-  measurementId: "G-9RZQ2G5NRF"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
